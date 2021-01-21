ac389874e1c78d450cfbb66383cd0e0aafd6693c
nhutnamhcmus <lenam.fithcmus@gmail.com>
2021-01-21 23:46:41 +0700

    intro2python and cert

---
+++ introduction-to-python/python-basics/script_01.py
@@ -0,0 +1,7 @@
+def sum(firstNumber, secondNumber):
+    return firstNumber + secondNumber
+# Example, do not modify!
+print(5 / 8)
+
+# Print the sum of 7 and 10
+print(sum(7,10))
---
+++ introduction-to-python/python-basics/script_05.py
@@ -0,0 +1,12 @@
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+years = 7
+result = savings * growth_multiplier ** years
+
+# Print out result
+print(result)
