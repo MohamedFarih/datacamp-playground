b43f7c14f0cbee1ba3d8dd40062838422fe5f053
nhutnamhcmus <lenam.fithcmus@gmail.com>
2021-02-14 23:22:05 +0700

    chap 3

---
+++ software-engineering-for-data-scientist-in-python/3. Utilizing Classes/my_script.py
@@ -0,0 +1,20 @@
+# Import custom text_analyzer package
+import text_analyzer
+
+# Create an instance of Document with datacamp_tweet
+my_document = text_analyzer.Document(text=datacamp_tweet)
+
+# Print the text attribute of the Document instance
+print(my_document.text)
+
+# Import custom text_analyzer package
+import text_analyzer
+
+# Create a SocialMedia instance with datacamp_tweets
+dc_tweets = text_analyzer.SocialMedia(text=datacamp_tweets)
+
+# Print the top five most most mentioned users
+print(dc_tweets.mention_counts.most_common(5))
+
+# Plot the most used hashtags
+text_analyzer.plot_counter(dc_tweets.hashtag_counts)
